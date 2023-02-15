8db33d15352f6947224dd277ed37b17bae1155cc
zh_bexultan <you@example.com>
2023-02-15 15:43:30 +0600

    Spring. Controllers Lesson - 2 example - 3 (id without ?=)

--- src/main/java/kz/bootcamp4/springboot/bootcamp4/springboot/controllers/HomeController.java
+++ src/main/java/kz/bootcamp4/springboot/bootcamp4/springboot/controllers/HomeController.java
@@ -8,6 +8,7 @@ import kz.bootcamp4.springboot.bootcamp4.springboot.db.Item;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -61,6 +62,11 @@ public class HomeController {
 
     @GetMapping(value="/details")
     public String details(@RequestParam(name="id") Long id, Model model) {
+        model.addAttribute("tovar", DBManager.getItem(id));
+        return "details";
+    }
+    @GetMapping(value="/details/{id}")
+    public String detailsView(@PathVariable(name="id") Long id, Model model) {
         model.addAttribute("tovar", DBManager.getItem(id));
         return "details";
     }
